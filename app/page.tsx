f3454d157fdf5a7124ef0669e88ae5c33ce34949
Priyam-magicalballoons <magicalballoons.priyam@gmail.com>
2025-05-15 14:33:34 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,153 +1,26 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel"
 import CustomInput from "@/components/ui/custumInput/CustomInput"
 import { Camera } from "lucide-react"
-import { useEffect, useRef, useState } from "react"
+import { redirect } from "next/navigation"
+import {  useState } from "react"
 
 
 const page = () => {
   const [name, setName] = useState("")
   const [other, setOther] = useState("")
-  const [isCapturing, setIsCapturing] = useState(false);
-  const imageRef = useRef<HTMLDivElement | null>(null);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [image, setImage] = useState<string | null>(null);
-  const [cameraMode, setCameraMode] = useState("environment")
-
-  const videoRef = useRef<HTMLVideoElement>(null);
-
-  useEffect(() => {
-    console.log(cameraMode)
-    if (typeof window !== "undefined" && navigator.mediaDevices?.getUserMedia) {
-      navigator.mediaDevices
-        .getUserMedia({
-          video: {
-            width: { ideal: 300 },
-            height: { ideal: 300 },
-            facingMode: "environment",
-          },
-          audio: false,
-        })
-        .then((stream) => {
-          if (videoRef.current) {
-            videoRef.current.srcObject = stream;
-          }
-        })
-        .catch((err) => {
-          console.error("Error accessing camera:", err);
-        });
-    } else {
-      console.error("Browser does not support getUserMedia");
-    }
-  }, [cameraMode]);
-
-  const captureImage = () => {
-    const video = videoRef.current;
-    const canvas = canvasRef.current;
-
-    if (!video || !canvas) return;
-
-    const context = canvas.getContext("2d");
-    if (!context) return;
-
-    // Set canvas size to match video dimensions
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-
-    // Draw the current video frame onto the canvas
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-    // Convert to base64 image
-    const dataUrl = canvas.toDataURL("image/png");
-    setImage(dataUrl);
-  };
+  
   return (
-    <div className="flex items-center justify-center w-full h-screen">
-      <div className="flex flex-col w-full md:w-1/2 lg:w-1/3 items-center gap-5 justify-start h-full">
+    <div className="flex items-center justify-center w-full h-screen flex-col py-10">
+      <div className="flex flex-col w-full md:w-1/2 lg:w-1/3 items-center gap-5 justify-start h-full flex-1">
+      <h2 className="text-3xl font-semibold">Fill the Details</h2>
       <CustomInput id="name" onchange={(e)=>setName(e as string)} placeholder="Enter doctor name" value={name} />
-      <CustomInput id="other" onchange={(e)=>setName(e as string)} placeholder="Enter Designation/Organisation/Speciality" value={name} />
-         <Button
-              variant="outline"
-              className={`self-center shadow-md py-10 w-[80%]  bg-gray-100 border-2 border-black`}
-              onClick={() => setIsCapturing(true)}
-            >
-              <Camera /> Capture Doctor Image
-            </Button>
-        {isCapturing && (
-            <div className="relative w-full max-w-[600px]" ref={imageRef}>
-              <img
-                src="/poster.png"
-                alt="Poster"
-                className="w-full h-auto block"
-              />
-              <div
-                className="absolute"
-                style={{
-                  top: "11.5%",
-                  left: "21%",
-                  width: "58%",
-                  aspectRatio: "1 / 1",
-                }}
-              >
-                <div className=" rounded overflow-hidden" onDoubleClick={()=>setCameraMode((mode)=>mode === "user" ? "environment" : "user")}>
-                  {image ? (
-                    <img
-                      src={image}
-                      alt="Captured"
-                      className="w-full h-full object-cover rounded-md block"
-                    />
-                  ) : (
-                    <video
-                      ref={videoRef}
-                      autoPlay
-                      muted
-                      playsInline
-                      className={`w-full h-full object-cover rounded-md ${
-                        image && "hidden"
-                      }`}
-                      style={{ display: "block" }}
-                    />
-                  )}
-                </div>
-              </div>
-              <div className={`w-full pt-5 flex justify-center ${image && "hidden"}`}>
-              <Button
-                onClick={captureImage}
-                className="px-4 py-2 bg-[#0c61aa] text-white rounded w-[80%] "
-                >
-                Capture Photo
-              </Button>
-                </div>
-              <canvas ref={canvasRef} style={{ display: "none" }} />
-            </div>
-          )}
-          {( image) && (
-            <div className=" py-2 flex flex-col gap-2 w-full items-center">
-              <Button
-                className="w-[80%] bg-red-400 hover:bg-red-00/70"
-                onClick={() => {
-                  setImage("")
-                  setIsCapturing(false)
-                }}
-              >
-                Retake Picture
-              </Button>
-              <Button className="w-[80%]"
-              //  onClick={handleDownload}
-               >
-                Download I-Pledge
-              </Button>
-            </div>
-          )}
+      <CustomInput id="other" onchange={(e)=>setOther(e as string)} placeholder="Enter Designation/Organisation/Speciality" value={other} />
       </div>
+        <div>
+      <Button onClick={()=>redirect(`/i-pledge?name=${name}&other=${other}`)}>Generate I-Pledge</Button>
+        </div>
     </div>
   )
 }
