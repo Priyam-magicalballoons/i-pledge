4dc0bf2ae0198bbbe6819ef3967ba5e3e2d2590d
Priyam-magicalballoons <magicalballoons.priyam@gmail.com>
2025-05-16 11:51:31 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,15 @@ import React from 'react'
 const page = () => {
   const router = useRouter()
   return (
-    <div className='w-full h-screen flex flex-col'>
+    <div className='w-full h-screen flex flex-col bg-[#fdd0cd] absolute'>
       <Image 
       src={"/initial-page.jpg"}
       alt='initial-page'
       height={200}
       width={400}
-      className=''
+      className='relative w-full'
       />
-      <div className='py-5 pt-10 w-full justify-center flex'>
+      <div className='py-5 pt-20 w-full justify-center flex h-full relative bottom-0 bg-[#fdd0cd]'>
       <Button onClick={()=>router.push(
         "/form"
       )} className='py-5 w-2/3 bg-red-700 active:bg-red-700/50 focus:bg-red-700'>
